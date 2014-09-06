9b67d536fed895a6f990c3e1a553f09193fa51fd
Vincent Ollivier <me@vinc.cc>
2014-09-06 15:32:49 +0200

    Rewrite cities script

--- app/assets/javascripts/cities.js
+++ app/assets/javascripts/cities.js
@@ -1,35 +1,41 @@
-var genMap = function(points) {
-  var map = L.map('map', {
+'use strict';
+
+function Map(id) {
+  this.map = L.map(id, {
     minZoom: 2,
     maxZoom: 6
   });
 
-  var markerOptions = {
-    radius: 8,
-    fillColor: '#9f3',
-    color: '#999',
-    weight: 1,
-    opacity: 1,
-    fillOpacity: 0.8
-  };
-
-  var markers = points.map(function(point) {
-    var location = point.city.location;
-
-    return L.geoJson(location, {
-      pointToLayer: function (feature, latlng) {
-        return L.circleMarker(latlng, markerOptions);
-      }
-    }).addTo(map);
-  });
-
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '<a href="http://osm.org/copyright">OpenStreetMap</a>'
-  }).addTo(map);
+  }).addTo(this.map);
 
-  if (markers.length) {
-    map.fitBounds(L.featureGroup(markers).getBounds(), { maxZoom: 4 });
-  } else {
-    map.setView([20, 0], 2);
-  }
-};
+  this.add = function(points, color) {
+    var map = this.map;
+
+    var markerOptions = {
+      radius: 8,
+      fillColor: color,
+      color: '#999',
+      weight: 1,
+      opacity: 1,
+      fillOpacity: 0.8
+    };
+
+    var markers = points.map(function(point) {
+      var location = point.city.location;
+
+      return L.geoJson(location, {
+        pointToLayer: function (feature, latlng) {
+          return L.circleMarker(latlng, markerOptions);
+        }
+      }).addTo(map);
+    });
+
+    if (markers.length) {
+      map.fitBounds(L.featureGroup(markers).getBounds(), { maxZoom: 4 });
+    } else {
+      map.setView([20, 0], 2);
+    }
+  };
+}
