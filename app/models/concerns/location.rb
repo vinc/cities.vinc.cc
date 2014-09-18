024e6a3665e32bf40904d289c7d4cea9c90d450f
Vincent Ollivier <me@vinc.cc>
2014-09-18 22:09:21 +0200

    Show neighbors of a city

--- app/models/concerns/location.rb
+++ app/models/concerns/location.rb
@@ -10,6 +10,12 @@ module Location
     self[:geo_near_distance].to_i
   end
 
+  def neighbors
+    self.class
+      .not.where(id: self.id)
+      .within_sphere(center: self.location, radius: 500 * 1000)
+  end
+
   module ClassMethods
     def within_sphere(center:, radius:)
       geo_near(center).max_distance(radius).spherical
